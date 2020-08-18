45dffa222c07501b293abef8bf93d04a7a087deb
Nick <neverate@gmail.com>
2020-08-18 18:40:13 +0300

    stage3: +RNG for stage3

--- src/bullscows/PseudoRandom.java
+++ src/bullscows/PseudoRandom.java
@@ -1,8 +1,22 @@
 package bullscows;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class PseudoRandom {
-    static int generateNumber(int digits){
-        String randomDigits = String.valueOf(System.nanoTime());
-        return 2;
+    static String generateNumber(int desiredLen){
+        StringBuilder result = new StringBuilder();
+        while (result.length() < desiredLen) {
+            List<String> randomDigits = String.valueOf(System.nanoTime())
+                .chars().distinct().mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.toList());
+            // who needs KISS when you've got this xD
+            randomDigits.removeAll(Arrays.asList(result.toString().split("")));
+            while (result.length() < desiredLen && randomDigits.size() > 0) {
+                result.append(randomDigits.remove(randomDigits.size() - 1));
+            }
+        }
+        return result.toString();
     }
 }
